296d3f6b9389070f9849246d008df6e46c54e09a
Minh Van Nguyen <mvngu.name@gmail.com>
2016-07-24 15:31:24 +1000

    issue #156: Get the internal function gnx_i_add_node_unweighted() to 100% coverage.

--- tests/base.c
+++ tests/base.c
@@ -897,6 +897,20 @@ add_node_no_memory(void)
     gnx_alloc_reset_limit();
 
     /**********************************************************************
+     * Directed and unweighted: cannot allocate memory for the set of
+     * in-neighbors.
+     *********************************************************************/
+
+    graph = gnx_new_full(GNX_DIRECTED, GNX_NO_SELFLOOP, GNX_UNWEIGHTED);
+    alloc_size = GNX_ALLOC_SET_SIZE + 1;
+    gnx_alloc_set_limit(alloc_size);
+    assert(!gnx_add_node(graph, &v));
+    assert(ENOMEM == errno);
+
+    gnx_destroy(graph);
+    gnx_alloc_reset_limit();
+
+    /**********************************************************************
      * undirected and unweighted
      *********************************************************************/
 
