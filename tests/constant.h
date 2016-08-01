1888027d71d1c80b21dd48a8e9576f8ef1c35f55
Minh Van Nguyen <mvngu.name@gmail.com>
2016-08-02 01:56:30 +1000

    issue #226: Rename a symbolic constant to make it more descriptive.

--- tests/constant.h
+++ tests/constant.h
@@ -35,11 +35,11 @@
 #define GNX_ALLOC_BUCKET_SIZE (2)
 
 /* The number of allocations that is required to add an entry to a bucket of a
- * set or dictionary.  This does not take into account the allocations to
- * possibly resize the bucket.  Any non-negative integer below this number
- * would result in an out-of-memory error.
+ * dictionary.  This does not take into account the allocations to possibly
+ * resize the bucket.  Any non-negative integer below this number would result
+ * in an out-of-memory error.
  */
-#define GNX_ALLOC_BUCKET_NODE_SIZE (1)
+#define GNX_ALLOC_DICT_BUCKET_NODE_SIZE (1)
 
 /* The number of allocations that is required to initialize a dictionary.  Any
  * non-negative integer below this number would result in an out-of-memory
