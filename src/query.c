857d5efa7f293b0c97db672e5446a111988eff6d
Minh Van Nguyen <mvngu.name@gmail.com>
2016-08-20 14:55:06 +1000

    issue #298: Add the function gnx_is_connected() to determine whether an undirected graph is connected.  We also provide tests.

---
+++ src/query.c
@@ -0,0 +1,129 @@
+/* gnx -- algorithms for graphs and networks
+ * Copyright (C) 2016 Minh Van Nguyen <mvngu.name AT gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
+
+#include <glib.h>
+
+#include "base.h"
+#include "query.h"
+#include "queue.h"
+#include "sanity.h"
+#include "set.h"
+#include "util.h"
+
+/**
+ * @file query.h
+ * @brief Tests for particular properties of graphs.
+ */
+
+/**************************************************************************
+ * public interface
+ *************************************************************************/
+
+/**
+ * @brief Determines whether an undirected graph is connected.
+ *
+ * An undirected graph @f$G@f$ is connected if there is a path between each
+ * pair of distinct nodes.  If we are given a node @f$u@f$ in @f$G@f$,
+ * then we should be able to reach another node @f$v@f$ in the same graph.
+ * Note that if @f$G@f$ has one node then @f$G@f$ is connected.  If @f$G@f$
+ * has zero nodes, then @f$G@f$ is not connected.
+ *
+ * @param graph Determine whether this undirected graph is connected.
+ * @return Nonzero if the graph is connected; zero otherwise.  If we are unable
+ *         to allocate memory, then @c errno is set to @c ENOMEM and we return
+ *         zero.
+ */
+int
+gnx_is_connected(GnxGraph *graph)
+{
+    GnxNeighborIter iter;
+    GnxQueue *queue = NULL;
+    GnxSet *seen = NULL;
+    gnxptr vptr;
+    int connected;
+    unsigned int start, *u, *v;
+
+    errno = 0;
+    gnx_i_check(graph);
+    g_return_val_if_fail(!graph->directed, GNX_FAILURE);
+
+    /* A graph with zero nodes is not connected. */
+    if (!graph->total_nodes)
+        return GNX_FAILURE;
+
+    /* A graph with one node is trivially connected. */
+    if (1 == graph->total_nodes)
+        return GNX_SUCCESS;
+
+    start = gnx_any_node(graph);
+
+    /* This will store the set of nodes that we have visited. */
+    seen = gnx_init_set();
+    if (!seen)
+        goto cleanup;
+    if (!gnx_set_add(seen, &start))
+        goto cleanup;
+
+    /* A queue of nodes to visit. */
+    queue = gnx_init_queue();
+    if (!queue)
+        goto cleanup;
+    if (!gnx_queue_append(queue, &start))
+        goto cleanup;
+
+    /* Use breadth-first search to determine connectivity. */
+    while (queue->size) {
+        u = gnx_queue_pop(queue);
+        g_assert(u);
+
+        /* Iterate over each (out-)neighbor of u.  Ignore the edge weights. */
+        gnx_neighbor_iter_init(&iter, graph, u);
+        while (gnx_neighbor_iter_next(&iter, &vptr, NULL)) {
+            v = (unsigned int *)vptr;
+            g_assert(v);
+
+            /* This should take care of nodes that we have seen.  Furthermore,
+             * it should take care of the case where u has a self-loop.
+             */
+            if (gnx_set_has(seen, v))
+                continue;
+
+            g_assert(*u != *v);
+            if (!gnx_set_add(seen, v))
+                goto cleanup;
+            if (!gnx_queue_append(queue, v))
+                goto cleanup;
+        }
+    }
+
+    connected = GNX_FAILURE;
+    if (seen->size == graph->total_nodes)
+        connected = GNX_SUCCESS;
+
+    gnx_destroy_queue(queue);
+    gnx_destroy_set(seen);
+
+    return connected;
+
+cleanup:
+    errno = ENOMEM;
+    gnx_destroy_queue(queue);
+    gnx_destroy_set(seen);
+    return GNX_FAILURE;
+}
---
+++ tests/query.c
@@ -0,0 +1,352 @@
+/* gnx -- algorithms for graphs and networks
+ * Copyright (C) 2016 Minh Van Nguyen <mvngu.name AT gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Test the functions in the module src/query.c. */
+
+#undef G_DISABLE_ASSERT
+
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <gnx.h>
+
+#include "routine.h"
+
+/**************************************************************************
+ * prototypes for internal helper functions
+ *************************************************************************/
+
+/* connected (undirected graph) */
+static void connected_disconnected(void);
+static void connected_empty(void);
+static void connected_MylesEtAl2011(void);
+static void connected_no_memory(void);
+static void connected_one_edge(void);
+static void connected_one_node(void);
+static void connected_Zachary1977(void);
+
+/**************************************************************************
+ * connected (undirected graph)
+ *************************************************************************/
+
+static void
+connected(void)
+{
+    connected_disconnected();
+    connected_empty();
+    connected_MylesEtAl2011();
+    connected_no_memory();
+    connected_one_edge();
+    connected_one_node();
+    connected_Zachary1977();
+}
+
+/* A connected graph will become disconnected after we add a node that is not
+ * in the graph.
+ */
+static void
+connected_disconnected(void)
+{
+    GnxGraph *graph;
+    unsigned int v;
+    const int low = 35;
+    const int high = 2 * low;
+    const unsigned int nnode = 34;
+    const unsigned int nedge = 78;
+
+    graph = gnx_read("data/network/karate.csv",
+                     GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_UNWEIGHTED);
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+
+    v = random_node_id(&low, &high);
+    assert(gnx_add_node(graph, &v));
+    assert(!gnx_is_connected(graph));
+    assert((nnode + 1) == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+
+    gnx_destroy(graph);
+}
+
+/* The empty graph is not connected.
+ */
+static void
+connected_empty(void)
+{
+    GnxGraph *graph;
+
+    /* Undirected, self-loop, unweighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_SELFLOOP, GNX_UNWEIGHTED);
+    is_empty_graph(graph);
+    assert(!gnx_is_connected(graph));
+    is_empty_graph(graph);
+    gnx_destroy(graph);
+
+    /* Undirected, self-loop, weighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_SELFLOOP, GNX_WEIGHTED);
+    is_empty_graph(graph);
+    assert(!gnx_is_connected(graph));
+    is_empty_graph(graph);
+    gnx_destroy(graph);
+
+    /* Undirected, no self-loop, unweighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_UNWEIGHTED);
+    is_empty_graph(graph);
+    assert(!gnx_is_connected(graph));
+    is_empty_graph(graph);
+    gnx_destroy(graph);
+
+    /* Undirected, no self-loop, weighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_WEIGHTED);
+    is_empty_graph(graph);
+    assert(!gnx_is_connected(graph));
+    is_empty_graph(graph);
+    gnx_destroy(graph);
+}
+
+/* An undirected graph of common grape cultivars.  The graph was adapted from
+ * (Myles et al. 2011).
+ *
+ * S. Myles, A. R. Boyko, C. L. Owens, P. J. Brown, F. Grassi,
+ *     M. K. Aradhya, B. Prins, A. Reynolds, J.-M. Chia, D. Ware,
+ *     C. D. Bustamante, and E. S. Buckler.  Genetic structure and
+ *     domestication history of the grape.  PNAS, 108:3530--3535, 2011.
+ */
+static void
+connected_MylesEtAl2011(void)
+{
+    GnxGraph *graph;
+    const unsigned int v = 48;
+    const unsigned int nnode = 54;
+    const unsigned int nedge = 80;
+
+    /* The wine network is connected. */
+    graph = gnx_read("data/network/wine.csv",
+                     GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_UNWEIGHTED);
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    assert(gnx_is_connected(graph));
+
+    /* Now disconnect the network. */
+    assert(gnx_delete_node(graph, &v));
+    assert(!gnx_is_connected(graph));
+
+    gnx_destroy(graph);
+}
+
+/* Test the function gnx_is_connected() under low-memory scenarios.
+ */
+static void
+connected_no_memory(void)
+{
+#ifdef GNX_ALLOC_TEST
+    GnxGraph *graph;
+    const unsigned int u = 0;
+    const unsigned int v = 1;
+
+    graph = gnx_new();
+    assert(gnx_add_edge(graph, &u, &v));
+
+    /* Cannot allocate memory for the set of visited nodes. */
+    gnx_alloc_set_limit(0);
+    assert(!gnx_is_connected(graph));
+    assert(ENOMEM == errno);
+
+    gnx_destroy(graph);
+    gnx_alloc_reset_limit();
+#endif
+}
+
+/* Whether a graph that has one edge is connected.
+ */
+static void
+connected_one_edge(void)
+{
+    GnxGraph *graph;
+    const double weight = 2.71828;
+    const unsigned int u = 0;
+    const unsigned int v = 1;
+    const unsigned int nnode = 2;
+    const unsigned int nedge = 1;
+
+    /* Undirected, self-loop, unweighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_SELFLOOP, GNX_UNWEIGHTED);
+    assert(gnx_add_edge(graph, &u, &u));
+    assert(gnx_add_node(graph, &v));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    assert(!gnx_is_connected(graph));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    gnx_destroy(graph);
+
+    /* Undirected, self-loop, weighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_SELFLOOP, GNX_WEIGHTED);
+    assert(gnx_add_edgew(graph, &u, &u, &weight));
+    assert(gnx_add_node(graph, &v));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    assert(!gnx_is_connected(graph));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    gnx_destroy(graph);
+
+    /* Undirected, no self-loop, unweighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_UNWEIGHTED);
+    assert(gnx_add_edge(graph, &u, &v));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    assert(gnx_is_connected(graph));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    gnx_destroy(graph);
+
+    /* Undirected, no self-loop, weighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_WEIGHTED);
+    assert(gnx_add_edgew(graph, &u, &v, &weight));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    assert(gnx_is_connected(graph));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+    gnx_destroy(graph);
+}
+
+/* An undirected graph that has one node is trivially connected.
+ */
+static void
+connected_one_node(void)
+{
+    GnxGraph *graph;
+    const double weight = 3.14159;
+    const unsigned int v = 0;
+
+    /* Undirected, self-loop, unweighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_SELFLOOP, GNX_UNWEIGHTED);
+    assert(gnx_add_edge(graph, &v, &v));
+    assert(1 == graph->total_nodes);
+    assert(1 == graph->total_edges);
+    assert(gnx_is_connected(graph));
+    assert(1 == graph->total_nodes);
+    assert(1 == graph->total_edges);
+    gnx_destroy(graph);
+
+    /* Undirected, self-loop, weighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_SELFLOOP, GNX_WEIGHTED);
+    assert(gnx_add_edgew(graph, &v, &v, &weight));
+    assert(1 == graph->total_nodes);
+    assert(1 == graph->total_edges);
+    assert(gnx_is_connected(graph));
+    assert(1 == graph->total_nodes);
+    assert(1 == graph->total_edges);
+    gnx_destroy(graph);
+
+    /* Undirected, no self-loop, unweighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_UNWEIGHTED);
+    assert(gnx_add_node(graph, &v));
+    assert(1 == graph->total_nodes);
+    assert(0 == graph->total_edges);
+    assert(gnx_is_connected(graph));
+    assert(1 == graph->total_nodes);
+    assert(0 == graph->total_edges);
+    gnx_destroy(graph);
+
+    /* Undirected, no self-loop, weighted. */
+    graph = gnx_new_full(GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_WEIGHTED);
+    assert(gnx_add_node(graph, &v));
+    assert(1 == graph->total_nodes);
+    assert(0 == graph->total_edges);
+    assert(gnx_is_connected(graph));
+    assert(1 == graph->total_nodes);
+    assert(0 == graph->total_edges);
+    gnx_destroy(graph);
+}
+
+/* The karate club network in (Zachary 1977).  The network has 34 nodes and
+ * 78 edges.  Note that the node IDs in the network starts from 1, not zero.
+ *
+ * W. W. Zachary.  An information flow model for conflict and fission
+ *     in small groups.  J. Anthropol. Res., 33:452--473, 1977.
+ */
+static void
+connected_Zachary1977(void)
+{
+    GnxGraph *graph;
+    unsigned int i, v;
+    const int low = 1;
+    const int high = 34;
+    const unsigned int nnode = 34;
+    const unsigned int nedge = 78;
+    const unsigned int nloop = (unsigned int)g_random_int_range(2, 8);
+
+    /**********************************************************************
+     * Undirected, no self-loop, unweighted.
+     *********************************************************************/
+
+    graph = gnx_read("data/network/karate.csv",
+                     GNX_UNDIRECTED, GNX_NO_SELFLOOP, GNX_UNWEIGHTED);
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+
+    assert(gnx_is_connected(graph));
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+
+    gnx_destroy(graph);
+
+    /**********************************************************************
+     * Undirected, self-loop, unweighted.
+     *********************************************************************/
+
+    graph = gnx_read("data/network/karate.csv",
+                     GNX_UNDIRECTED, GNX_SELFLOOP, GNX_UNWEIGHTED);
+    assert(nnode == graph->total_nodes);
+    assert(nedge == graph->total_edges);
+
+    /* Insert a bunch of self-loops. */
+    for (i = 0; i < nloop; i++) {
+        do {
+            v = random_node_id(&low, &high);
+        } while (gnx_has_edge(graph, &v, &v));
+
+        assert(gnx_add_edge(graph, &v, &v));
+    }
+    assert(nnode == graph->total_nodes);
+    assert((nedge + nloop) == graph->total_edges);
+
+    assert(gnx_is_connected(graph));
+    assert(nnode == graph->total_nodes);
+    assert((nedge + nloop) == graph->total_edges);
+
+    gnx_destroy(graph);
+}
+
+/**************************************************************************
+ * start here
+ *************************************************************************/
+
+int
+main(int argc,
+     char *argv[])
+{
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_add_func("/query/connected", connected);
+
+    return g_test_run();
+}
